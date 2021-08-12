480e796b157f05e62369ad7d7a62f6285ae7f9e9
Munkh-Erdene <moogii67890@gmail.com>
2021-08-12 12:26:38 +0800

    moogii light repo

--- src/components/Banner/AppBanner.js
+++ src/components/Banner/AppBanner.js
@@ -12,33 +12,23 @@ const AppBanner = () => {
             <Reveal bottom cascade>
               <div className="app_banner_contentmt mt_40">
                 <h2
-                  className="f_p f_700 f_size_50 w_color mb_20 wow fadeInLeft"
+                  className="f_p f_900 f_size_70 w_color mb_20 wow fadeInLeft"
                   data-wow-delay="0.2s"
                 >
-                  Faro Limited Group
+                  Faro <br/>Limited<br/> Group
                   <br />
                 </h2>
                 <p
                   className="f_400 f_size_18 l_height30 w_color wow fadeInLeft"
                   data-wow-delay="0.3s"
                 >
-                  Faro limited group. Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate
-                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                  sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
+                  THE LIGHT TOWER FOR THE FUTURE
                 </p>
               </div>
             </Reveal>
           </div>
           <div className="col-lg-6 light-house" >
-            {/* <center> */}
-
-            <div className="app_img">
-                
+              <div className="app_img">
               {/* <img
                 className="app_screen one wow fadeInDown"
                 src={require("../../img/home7/light-tower.png")}
@@ -56,20 +46,16 @@ const AppBanner = () => {
               /> */}
               <div className="light-house-back"/>
               <img
-                className="light-house"
                 src={require("../../img/home7/light-tower.png")}
                 alt=""
-              />
-            </div>
-            {/* </center> */}
-
-          </div>
-        </div>
-        
-      </div>
-      <div className="box">
+              /><div className="box">
                   <img src={light} className="light" />
                 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </section>
   );
 };
